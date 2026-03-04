b20c1715ce2f872c6445727143a9f3ecf7b4d433
Gleb <gleb.homa6@gmail.com>
2026-03-04 14:32:14 +0800

    Added new test fuanction

--- testing/tests/test_principle.py
+++ testing/tests/test_principle.py
@@ -1,13 +1,25 @@
 import sys
+from urllib.response import addbase
 
-#TODO make with 'pip install -e
+#TODO make with 'pip install -e'
 sys.path.append("../src")
 
-from math demo import add
+from math demo import add, add_with_bug
 
 def test_addition():
     assert add(2, 2) == 4
     print("Tests passed")
+
+def test_bug_addition_notsufficient():
+    assert add_with_bug(2, 2) == 4
+    print("Test BUG ADDITION PASSED")
+
+def test_bug_addition_enough():
+    assert add_with_bug(2, 2) == 4
+    assert add_with_bug(0, 0) == 0
+    assert add_with_bug(5, 6) == 11
+    print("Test BUG ADDITION PASSED")
+
 
 if __name__ == '__main__':
     test_addition()
