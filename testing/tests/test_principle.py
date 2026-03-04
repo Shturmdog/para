c087b97dd978b306c5d63a3a98cabe6f17865b2e
Gleb <gleb.homa6@gmail.com>
2026-03-04 15:00:08 +0800

    Added new test function

--- testing/tests/test_principle.py
+++ testing/tests/test_principle.py
@@ -4,7 +4,7 @@ from urllib.response import addbase
 #TODO make with 'pip install -e'
 sys.path.append("../src")
 
-from math demo import add, add_with_bug
+from math_demo import (add, add_with_bug, add_something)
 
 def test_addition():
     assert add(2, 2) == 4
@@ -27,9 +27,39 @@ def test_addition_duplicated_logic():
     assert add(6, 3) == 9
     print("Test DUPLICATION LOGIC PASSED")
 
+def test_addition_overcomplicated():
+    for i in range(0, 2**32):
+        for j in range(0, 2**32):
+            assert add(i, j) == i + j #bad since violated "duplicated"
+            assert add(-i, j) == -i + j
+            assert add(i, -j) == i - j
+            assert add(-i, -j) == -i - j
+
+def test_addition_resonable():
+    assert add(6, 3) == 9
+    assert add(0, 3) == 3
+    assert add(0, -3) == -3
+    assert add(-7, 83) == 76
+    assert add(-7, -83) == -90
+
+def test_add_something_resonable():
+    assert add_something(6, 3) == 9
+    assert add_something(None, None) == 0
+    assert add_something(None, "abc") == 0
+    assert add_something(None, 10) == 0
+    assert add_something("abc", 10) == "abc10"
+    assert add_something("10", "abc") == "10abc"
+    assert add_something("xyz", "abc") == "xyzabc"
+    print("TESTS PASSED")
+    
+
+
+
 
 if __name__ == '__main__':
     test_addition()
     test_bug_addition_notsufficient()
-    test_addition_enough()
+    test_bug_addition_enough()
     test_addition_duplicated_logic()
+    test_addition_resonable()
+    test_add_something_resonable()
